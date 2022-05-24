81649ac6f9c288013af05ab6debea76c3ab053e5
jbgoldman1104 <jbgoldman1104@gmail.com>
2022-05-24 10:11:58 -0800

    "updated on 2022-05-24"

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import (
 	"github.com/marcio-olmedo-cavalini/financial-transactions-go-webapp/database"
 	"github.com/marcio-olmedo-cavalini/financial-transactions-go-webapp/models"
+	"github.com/marcio-olmedo-cavalini/financial-transactions-go-webapp/routes"
+)
+
+func main() {
+	database.OpenConnection()
+	Migrate()
 	routes.HandleRequests()
 }
 
