1c138570acdae98e0e596bf2bafb417fac414364
jbgoldman1104 <jbgoldman1104@gmail.com>
2022-06-08 10:12:00 -0800

    "updated on 2022-06-08"

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+package main
 
 import (
 	"github.com/marcio-olmedo-cavalini/financial-transactions-go-webapp/database"
@@ -7,13 +8,3 @@ import (
 
 func main() {
 	database.OpenConnection()
-	Migrate()
-	routes.HandleRequests()
-}
-
-func Migrate() {
-	database.DB.AutoMigrate(&models.FinancialTransaction{})
-	database.DB.AutoMigrate(&models.TransactionReport{})
-	database.DB.AutoMigrate(&models.User{})
-	database.DB.Exec("INSERT INTO users (created_at, updated_at, nome, email, password) select now() as created_at, now() as updated_at, 'Admin' as nome, 'admin@admin.com' as email, '$2a$14$jB541M/dA8PXeHqObePUx.2JfYWFKtEoRhAQjcnvqC4DPqFcGVWUW' as password where not exists (select id from users where nome = 'Admin')")
-}
